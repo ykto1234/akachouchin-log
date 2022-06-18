15a523a11e83ea12faedcd56952f5c1d818c971d
ykto1234 <to.46101753@gmail.com>
2022-06-18 18:12:53 +0900

    add: font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserHomeComponent } from './pages/user-home/user-home.component';
-import { UserHeaderComponent } from './pages/user-header/user-header.component';
-import { environment } from 'src/environments/environment';
-import { AngularFireModule } from '@angular/fire/compat';
-import { ShopCardComponent } from './components/shop-card/shop-card.component';
-import { UserHeroComponent } from './pages/user-hero/user-hero.component';
 import { SearchConditionPrefectureComponent } from './components/search-condition-prefecture/search-condition-prefecture.component';
+import { ShopCardComponent } from './components/shop-card/shop-card.component';
 import { AreaShopListComponent } from './pages/area-shop-list/area-shop-list.component';
+import { UserHeaderComponent } from './pages/user-header/user-header.component';
+import { UserHeroComponent } from './pages/user-hero/user-hero.component';
+import { UserHomeComponent } from './pages/user-home/user-home.component';
+
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { AreaShopListComponent } from './pages/area-shop-list/area-shop-list.com
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
