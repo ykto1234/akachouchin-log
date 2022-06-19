38e498713c1f0ed00fb86b71526670eccb1f36b8
ykto1234 <to.46101753@gmail.com>
2022-06-19 14:24:39 +0900

    add: area-shop-list paginator

--- src/app/service/shop.service.ts
+++ src/app/service/shop.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  DocumentData,
+  QueryDocumentSnapshot,
+} from '@angular/fire/compat/firestore';
 import { cloneDeep } from 'lodash-es';
 import { environment } from 'src/environments/environment';
 import { ShopInfo } from '../interface/shop-info';
@@ -36,22 +40,85 @@ export class ShopInfoService {
     });
   }
 
-  getShopsByAreaCode(areaCode: string, limit: number = 1000): void {
+  async getShopsByAreaCode(
+    areaCode: string,
+    limit: number = 100
+  ): Promise<ShopInfo[]> {
     const query = this.versionDoc.collection('shops', (ref: any) =>
       ref
         .where('areaCode', '==', areaCode)
         .orderBy('valuationRates', 'desc')
         .limit(limit)
     );
-    query.get().subscribe((querySnapshot) => {
-      const shops = querySnapshot.docs.map((doc) => {
+
+    const snapShots = await query.get();
+    if (!snapShots) {
+      return [];
+    }
+    let shops: ShopInfo[] = [];
+    let docSnapshots: QueryDocumentSnapshot<DocumentData>[] = [];
+    await snapShots.forEach((snapShot) => {
+      docSnapshots = cloneDeep(snapShot.docs);
+      shops = snapShot.docs.map((doc) => {
         const id = doc.id;
         const data: any = doc.data();
         const shop: ShopInfo = { id: id, ...data };
         return shop;
       });
-      this.shopAreaStore.update(cloneDeep(shops));
     });
+    this.shopAreaStore.update({
+      shops: cloneDeep(shops),
+      docSnapshots: docSnapshots,
+      pageIndex: 0,
+    });
+    return shops;
+  }
+
+  async getNextShopsByAreaCode(
+    areaCode: string,
+    startAfter: QueryDocumentSnapshot<DocumentData> | null,
+    pageIndex: number,
+    shops: ShopInfo[],
+    docSnapshots: QueryDocumentSnapshot<DocumentData>[],
+    limit: number = 10
+  ): Promise<ShopInfo[]> {
+    const query = this.versionDoc.collection('shops', (ref: any) =>
+      ref
+        .where('areaCode', '==', areaCode)
+        .orderBy('valuationRates', 'desc')
+        .startAfter(startAfter)
+        .limit(limit + 1)
+    );
+
+    const snapShots = await query.get();
+    if (!snapShots) {
+      return [];
+    }
+    await snapShots.forEach((snapShot) => {
+      docSnapshots.concat(cloneDeep(snapShot.docs));
+      shops.concat(
+        snapShot.docs.map((doc) => {
+          const id = doc.id;
+          const data: any = doc.data();
+          const shop: ShopInfo = { id: id, ...data };
+          return shop;
+        })
+      );
+    });
+
+    this.shopAreaStore.update({
+      shops: cloneDeep(shops),
+      docSnapshots: cloneDeep(docSnapshots),
+      pageIndex: pageIndex,
+    });
+    return shops;
+  }
+
+  getPrevShopsByAreaCode(pageIndex: number): void {
+    this.shopAreaStore.pageUpdate({
+      pageIndex: pageIndex,
+    });
+    return;
   }
 
   getShopsByGenreId(genreId: string, limit: number = 1000): void {
--- src/app/store/shop-area-store.ts
+++ src/app/store/shop-area-store.ts
@@ -1,24 +1,62 @@
 import { Injectable } from '@angular/core';
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+} from '@angular/fire/compat/firestore';
 import { ComponentStore } from '@ngrx/component-store';
 import { Observable } from 'rxjs';
 import { ShopInfo } from '../interface/shop-info';
 
 export interface ShopAreaState {
-  shops: ShopInfo[];
+  shops?: ShopInfo[];
+  docSnapshots?: QueryDocumentSnapshot<DocumentData>[];
+  pageIndex?: number;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShopAreaStore extends ComponentStore<ShopAreaState> {
-  readonly shopAreaInfo$: Observable<ShopInfo[]> = this.select(
-    (state) => state.shops
+  readonly shopAreaInfo$: Observable<{
+    shops?: ShopInfo[];
+    docSnapshots?: QueryDocumentSnapshot<DocumentData>[];
+    pageIndex?: number;
+  }> = this.select((state) => {
+    return {
+      shops: state.shops,
+      docSnapshots: state.docSnapshots,
+      pageIndex: state.pageIndex,
+    };
+  });
+  readonly update = this.updater(
+    (
+      _,
+      value: {
+        shops: ShopInfo[];
+        docSnapshots: QueryDocumentSnapshot<DocumentData>[];
+        pageIndex: number;
+      }
+    ) => ({
+      shops: value.shops,
+      docSnapshots: value.docSnapshots,
+      pageIndex: value.pageIndex,
+    })
   );
-  readonly update = this.updater((_, shops: ShopInfo[]) => ({
-    shops,
-  }));
+
+  readonly pageUpdate = this.updater(
+    (
+      _,
+      value: {
+        pageIndex: number;
+      }
+    ) => ({
+      shops: _.shops,
+      docSnapshots: _.docSnapshots,
+      pageIndex: value.pageIndex,
+    })
+  );
 
   constructor() {
-    super({ shops: [] });
+    super({ shops: [], docSnapshots: [], pageIndex: 0 });
   }
 }
